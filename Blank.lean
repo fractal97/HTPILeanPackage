a7affff3fc72a37bfda828d306c01724cefcf7cb
Vin <fractal97@hotmail.com>
2025-12-18 17:51:47 -0600

    Add theorems Example_3_2_4 and two_imp to HTPI namespace

--- Blank.lean
+++ Blank.lean
@@ -1,2 +1,24 @@
 import HTPILib.HTPIDefs
 namespace HTPI
+
+theorem Example_3_2_4
+    (P Q R : Prop) (h : P → (Q → R)) : ¬R → (P → ¬Q) := by
+  assume h2 : ¬R
+  assume h3 : P
+  have h4 : Q → R := h h3
+  contrapos at h4            --Now h4 : ¬R → ¬Q
+  show ¬Q from h4 h2
+  done
+
+
+theorem two_imp (P Q R : Prop)
+    (h1 : P → Q) (h2 : Q → ¬R) : R → ¬P := by
+    contrapos
+    assume h3: P
+    have h4:=h1 h3
+    show ¬R from h2 h4
+    done
+
+
+
+end HTPI
